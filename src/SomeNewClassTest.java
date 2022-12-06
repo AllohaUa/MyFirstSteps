1af979a2851edf01bcd250fbe52c9444f0bea5de
Dima <you@example.com>
2022-12-06 16:11:01 +0200

    add new file

---
+++ src/SomeNewClassTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeNewClassTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showText() {
+    }
+}
